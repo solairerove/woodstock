c3e1f8634b4059214f1fbfde870f6ce1d210c952
solairerove <hax2033@gmail.com>
2016-09-26 21:45:14 +0300

    Parse and render list of persons

--- src/main/webapp/app/components/person-list/person-list.controller.js
+++ src/main/webapp/app/components/person-list/person-list.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
-woodstock.controller('PersonListController', ['$route', 'PersonService', function ($route, PersonService) {
-    PersonService.query(function (result) {
-        this.persons = result;
-    })
-}]);
+woodstock.controller('PersonListController', ['$scope', '$route', 'PersonService',
+    function ($scope, $route, PersonService) {
+        PersonService.get(function (data) {
+            $scope.persons = data._embedded.personList;
+        })
+    }
+]);
