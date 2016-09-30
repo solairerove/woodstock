21b2a1fcd96134dffc150e98cfab98eb42753d81
solairerove <hax2033@gmail.com>
2016-09-30 23:51:58 +0300

    Implement profile service tests: - update travis.yaml

--- src/test/java/com/github/solairerove/woodstock/service/ProfileServiceTest.java
+++ src/test/java/com/github/solairerove/woodstock/service/ProfileServiceTest.java
@@ -5,11 +5,16 @@ import com.github.solairerove.woodstock.domain.Profile;
 import com.github.solairerove.woodstock.repository.ProfileRepository;
 import com.github.solairerove.woodstock.utils.EntityUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by krivitski-no on 9/28/16.
@@ -17,12 +22,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 public class ProfileServiceTest {
+    private static final int NUMBER_OF_ENTITIES_IN_COLLECTION = 10;
 
     @Autowired
     private ProfileService profileService;
 
     @Autowired
     private ProfileRepository profileRepository;
+
+    @Before
+    public void setUp() {
+        profileRepository.deleteAll();
+    }
 
     @Test
     public void createProfileTest() {
@@ -49,5 +60,37 @@ public class ProfileServiceTest {
         profileService.updateProfile(saved.getId(), updated);
 
         Assert.assertEquals(updated.getFirstName(), profileRepository.findOne(saved.getId()).getFirstName());
+    }
+
+    @Test
+    public void deleteProfileTest() {
+        Profile saved = EntityUtils.generateProfile();
+        profileRepository.save(saved);
+
+        profileService.deleteProfile(saved.getId());
+
+        Assert.assertEquals(profileRepository.findOne(saved.getId()), null);
+    }
+
+    @Test
+    public void deleteProfileByEntityTest() {
+        Profile saved = EntityUtils.generateProfile();
+        profileRepository.save(saved);
+
+        profileService.deleteProfile(saved);
+
+        Assert.assertEquals(profileRepository.findOne(saved.getId()), null);
+    }
+
+    @Test
+    public void findAllTest() {
+        Collection<Profile> profiles = new ArrayList<>();
+        for(int i = 0; i < NUMBER_OF_ENTITIES_IN_COLLECTION; i++) {
+            profiles.add(EntityUtils.generateProfile());
+        }
+        profileRepository.save(profiles);
+
+        Assert.assertEquals(NUMBER_OF_ENTITIES_IN_COLLECTION,
+                profileService.findAll(new PageRequest(0, NUMBER_OF_ENTITIES_IN_COLLECTION)).getTotalElements());
     }
 }
