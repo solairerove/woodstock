39911565863fdea572fc3a13a8c1fac2f69eeeb2
solairerove <hax2033@gmail.com>
2016-10-02 17:11:40 +0300

    Implement few profile controller tests

--- src/test/java/com/github/solairerove/woodstock/controller/ProfileControllerTest.java
+++ src/test/java/com/github/solairerove/woodstock/controller/ProfileControllerTest.java
@@ -1,7 +1,76 @@
 package com.github.solairerove.woodstock.controller;
 
+import com.github.solairerove.woodstock.Application;
+import com.github.solairerove.woodstock.domain.Profile;
+import com.github.solairerove.woodstock.repository.ProfileRepository;
+import com.github.solairerove.woodstock.service.ProfileService;
+import com.github.solairerove.woodstock.utils.EntityUtils;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 /**
  * Created by krivitski-no on 9/28/16.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest(classes = Application.class)
 public class ProfileControllerTest {
+    private static final String API_PATH = "/api/profiles";
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @Autowired
+    private ProfileService profileService;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        profileRepository.deleteAll();
+    }
+
+    @Test
+    public void getAllProfilesTest() throws Exception {
+        profileRepository.save(EntityUtils.generateProfileCollection());
+
+        mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, API_PATH)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$._embedded.profileList", Matchers.hasSize(EntityUtils.NUMBER_OF_ENTITIES_IN_COLLECTION)));
+    }
+
+    @Test
+    public void getProfileTest() throws Exception {
+        Profile profile = EntityUtils.generateProfile();
+        profileRepository.save(profile);
+        String id = profile.getId();
+
+        mockMvc.perform(MockMvcRequestBuilders.request(HttpMethod.GET, API_PATH + "/" + id)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id", Matchers.is(id)));
+    }
 }
