b1614309a92f9fefcb76deb30dd231cca723933e
solairerove <hax2033@gmail.com>
2016-10-22 16:04:36 +0300

    Create base dev maven profile: - create uri property - prepare to surefire plugin - may be groovy? :)

--- src/main/java/com/github/solairerove/woodstock/config/Neo4jConfig.java
+++ src/main/java/com/github/solairerove/woodstock/config/Neo4jConfig.java
@@ -1,6 +1,7 @@
 package com.github.solairerove.woodstock.config;
 
 import org.neo4j.ogm.session.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
@@ -12,12 +13,15 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 public class Neo4jConfig extends Neo4jConfiguration {
 
+    @Value("${spring.data.neo4j.uri}")
+    private String URI;
+
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
         org.neo4j.ogm.config.Configuration configuration = new org.neo4j.ogm.config.Configuration();
         configuration.driverConfiguration()
                 .setDriverClassName("org.neo4j.ogm.drivers.http.driver.HttpDriver")
-                .setURI("http://neo4j:woodstock@localhost:7474");
+                .setURI(URI);
 
         return configuration;
     }
