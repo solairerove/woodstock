90ee54571396422165e036faa21facec720ae6c7
solairerove <hax2033@gmail.com>
2016-10-28 22:05:14 +0300

    Create separate query: - maybe improve import.cypher file

--- additional/import/import.py
+++ additional/import/import.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3.4
 
+import time
 from py2neo import Graph
 
-host_port = "localhost:7474"
-user_name = "neo4j"
-password = "woodstock"
+def main():
 
-graph = Graph("http://" + user_name + ":" + password + "@" + host_port + "/db/data/")
+    host_port = "localhost:7474"
+    user_name = "neo4j"
+    password = "woodstock"
 
-print(graph.run("MATCH (n) RETURN n LIMIT 200").dump())
+    graph = Graph("http://" + user_name + ":" + password + "@" + host_port + "/db/data/")
+
+    query = '''
+        MATCH (n) RETURN n LIMIT 200
+    '''
+
+    print(graph.run(query).dump())
+
+if __name__ == '__main__':
+    start = time.time()
+    main()
+    end = time.time() - start
+    print("Time to complete:", end)
