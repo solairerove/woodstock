590595af2bd6d943d54ba34dac2bdf6866f3b196
solairerove <hax2033@gmail.com>
2016-11-02 09:34:14 +0300

    Rename ticket component folder: - update route - rename ticket controller - update index

--- src/main/resources/static/app/components/ticket/route.js
+++ src/main/resources/static/app/components/ticket/route.js
@@ -3,11 +3,11 @@
 woodstock.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
     $routeProvider
         .when('/tasks/:taskId/tickets', {
-            templateUrl: 'app/components/ticket-list/ticket-list.html',
+            templateUrl: 'app/components/ticket/ticket-list.html',
             controller: 'TicketListController'
         })
         .when('/tasks/:taskId/tickets/:ticketId/view', {
-            templateUrl: 'app/components/ticket-list/ticket-view.html',
+            templateUrl: 'app/components/ticket/ticket-view.html',
             controller: 'TicketViewController'
         })
 }]);
