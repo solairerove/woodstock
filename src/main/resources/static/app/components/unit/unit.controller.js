63fb0ca002d0901fca3eeb6a1b2c4e923497fc49
solairerove <hax2033@gmail.com>
2016-11-03 11:25:34 +0300

    Create simple template: - fix category list component back button

--- src/main/resources/static/app/components/unit/unit.controller.js
+++ src/main/resources/static/app/components/unit/unit.controller.js
@@ -5,10 +5,6 @@ woodstock.controller('UnitListController', ['$scope', '$location', '$routeParams
         UnitService.query(function (data) {
             $scope.units = data;
         });
-
-        $scope.viewUnit = function (unitId) {
-            $scope.unit = UnitService.get({unitId: unitId});
-        }
     }
 ]).controller('UnitViewController', ['$scope', '$location', '$routeParams', 'UnitService',
     function ($scope, $location, $routeParams, UnitService) {
