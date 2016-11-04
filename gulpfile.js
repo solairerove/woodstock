98a91b8f5db7e425340d7e3f3acc22a1d084dcf2
solairerove <hax2033@gmail.com>
2016-11-04 15:51:06 +0300

    Move view to webapp folder: - update unit component

--- gulpfile.js
+++ gulpfile.js
@@ -15,21 +15,21 @@ gulp.task('lib-js-files', function () {
     vendorJs = gulp.src(mainBowerFiles('**/*.js'), {base: 'bower_components'})
         // .pipe(concatVendor('lib.min.js'))
         // .pipe(uglify())
-        .pipe(gulp.dest('src/main/resources/static/resources/vendor/js'));
+        .pipe(gulp.dest('src/main/webapp/resources/vendor/js'));
 });
 
 gulp.task('lib-css-files', function () {
     vendorCss = gulp.src(mainBowerFiles('**/*.css'), {base: 'bower_components'})
         // .pipe(concat('lib.min.css'))
         // .pipe(minify())
-        .pipe(gulp.dest('src/main/resources/static/resources/vendor/css'));
+        .pipe(gulp.dest('src/main/webapp/resources/vendor/css'));
 });
 
 gulp.task('index', function () {
-    var target = gulp.src("src/main/resources/static/index.html");
-    var sources = gulp.src(['src/main/resources/static/app/**/*.js', 'src/main/resources/static/resources/css/**/*.css'], {read: false});
+    var target = gulp.src("src/main/webapp/index.html");
+    var sources = gulp.src(['src/main/webapp/app/**/*.js', 'src/main/webapp/resources/css/**/*.css'], {read: false});
     return target.pipe(inject(series(vendorJs, vendorCss, sources), {relative: true}))
-        .pipe(gulp.dest('src/main/resources/static/'));
+        .pipe(gulp.dest('src/main/webapp/'));
 });
 
 // gulp.task('copyFonts', function() {
