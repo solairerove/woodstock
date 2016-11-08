644e98d2c599f42910148d17e64d7e5da1c5b467
solairerove <hax2033@gmail.com>
2016-11-08 19:36:28 +0300

    Update python import

--- additional/import/import.py
+++ additional/import/import.py
@@ -18,12 +18,41 @@ def main():
 
     graph.delete_all()
 
+    java_description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus suscipit mollis.' \
+                       'Vivamus dignissim quis arcu et consectetur. Nulla volutpat ut metus sit amet efficitur. ' \
+                       'Donec molestie orci orci, ac scelerisque metus vehicula a.'
+
+    java_reference = 'Ut a nisi sit amet turpis euismod rhoncus. Phasellus hendrerit vel turpis ut condimentum. ' \
+                     'Nam cursus turpis id ex sagittis, nec aliquam risus rhoncus. ' \
+                     'Vivamus fringilla erat in tincidunt rutrum. ' \
+                     'Sed vulputate purus non lorem blandit, ut consectetur eros egestas.'
+
+    python_description = 'Nulla sollicitudin iaculis velit, at ultricies arcu malesuada sed. ' \
+                         'Pellentesque eu euismod nunc, in consequat sem. Aliquam pulvinar ante in lobortis faucibus. ' \
+                         'Fusce consectetur nibh elit, nec egestas ipsum imperdiet et. ' \
+                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
+    python_reference = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus suscipit mollis.' \
+                     'Vivamus dignissim quis arcu et consectetur. Nulla volutpat ut metus sit amet efficitur. ' \
+                     'Donec molestie orci orci, ac scelerisque metus vehicula a.'
+
+    js_description = 'Nulla sollicitudin iaculis velit, at ultricies arcu malesuada sed. ' \
+                         'Pellentesque eu euismod nunc, in consequat sem. Aliquam pulvinar ante in lobortis faucibus. ' \
+                         'Fusce consectetur nibh elit, nec egestas ipsum imperdiet et. ' \
+                         'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
+    js_reference = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus suscipit mollis.' \
+                       'Vivamus dignissim quis arcu et consectetur. Nulla volutpat ut metus sit amet efficitur. ' \
+                       'Donec molestie orci orci, ac scelerisque metus vehicula a.'
+
     # units start
-    java = Node("Unit", label="Java")
-    python = Node("Unit", label="Python")
+    java = Node("Unit", label="Java", description=java_description, reference=java_reference)
+    python = Node("Unit", label="Python", description=python_description, reference=python_reference)
+    java_script = Node("Unit", label="Java Script", description=js_description, reference=js_reference)
 
     transaction.create(java)
     transaction.create(python)
+    transaction.create(java_script)
     # units end
 
     java_categories = [
@@ -40,6 +69,11 @@ def main():
     python_categories = [
         "Some python category 1",
         "Some python category 2"
+    ]
+
+    java_script_categories = [
+        "Some js category 1",
+        "Some js category 2"
     ]
 
     # category java
@@ -83,6 +117,28 @@ def main():
                 transaction.create(ticket_python)
 
                 transaction.create(Relationship(ticket_python, "HAS_IN", task_python))
+
+
+    # category js
+    for category in java_script_categories:
+        category_js = Node("Category", name=category)
+        transaction.create(category_js)
+
+        transaction.create(Relationship(category_js, "HAS_IN", java_script))
+
+        # task js
+        for x in range(0, 15):
+            task_js = Node("Task", question="Some question " + str(x))
+            transaction.create(task_js)
+
+            transaction.create(Relationship(task_js, "HAS_IN", category_js))
+
+            # ticket js
+            for y in range(0, 10):
+                ticket_js = Node("Ticket", value="Some ticket" + str(y))
+                transaction.create(ticket_js)
+
+                transaction.create(Relationship(ticket_js, "HAS_IN", task_js))
 
     transaction.commit()
 
