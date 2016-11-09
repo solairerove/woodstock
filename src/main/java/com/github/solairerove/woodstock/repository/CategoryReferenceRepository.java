86ae699876f8a216b610ac67550b308e4a97cae9
solairerove <hax2033@gmail.com>
2016-11-09 14:11:18 +0300

    Add demo repositories: - may be generic in future?

---
+++ src/main/java/com/github/solairerove/woodstock/repository/CategoryReferenceRepository.java
@@ -0,0 +1,8 @@
+package com.github.solairerove.woodstock.repository;
+
+import com.github.solairerove.woodstock.domain.CategoryReference;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface CategoryReferenceRepository extends GraphRepository<CategoryReference> {
+
+}
---
+++ src/main/java/com/github/solairerove/woodstock/repository/UnitChapterRepository.java
@@ -0,0 +1,8 @@
+package com.github.solairerove.woodstock.repository;
+
+import com.github.solairerove.woodstock.domain.UnitChapter;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface UnitChapterRepository extends GraphRepository<UnitChapter> {
+
+}
