546dcf87ca7cba6a18efbfe6c865cb8eb287dd4c
solairerove <hax2033@gmail.com>
2016-11-11 17:05:57 +0300

    Create chapter service stubs: - it should be generic maybe - or simple inheritance

--- src/main/java/com/github/solairerove/woodstock/service/ChapterService.java
+++ src/main/java/com/github/solairerove/woodstock/service/ChapterService.java
@@ -1,4 +1,13 @@
 package com.github.solairerove.woodstock.service;
 
+import com.github.solairerove.woodstock.domain.Chapter;
+import com.github.solairerove.woodstock.dto.ChapterDTO;
+
 public interface ChapterService {
+
+    Chapter create(Long refId, ChapterDTO chapterDTO);
+
+    Chapter get(Long refId, Long chapterId);
+
+    Iterable<Chapter> getAll(Long refId);
 }
--- src/main/java/com/github/solairerove/woodstock/service/impl/ChapterServiceImpl.java
+++ src/main/java/com/github/solairerove/woodstock/service/impl/ChapterServiceImpl.java
@@ -1,5 +1,7 @@
 package com.github.solairerove.woodstock.service.impl;
 
+import com.github.solairerove.woodstock.domain.Chapter;
+import com.github.solairerove.woodstock.dto.ChapterDTO;
 import com.github.solairerove.woodstock.repository.ChapterRepository;
 import com.github.solairerove.woodstock.service.ChapterService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +10,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ChapterServiceImpl implements ChapterService {
 
-    private final ChapterRepository repository;
+    private final ChapterRepository chapterRepository;
 
     @Autowired
-    public ChapterServiceImpl(ChapterRepository repository) {
-        this.repository = repository;
+    public ChapterServiceImpl(ChapterRepository chapterRepository) {
+        this.chapterRepository = chapterRepository;
+    }
+
+    @Override
+    public Chapter create(Long refId, ChapterDTO chapterDTO) {
+
+        return null;
+    }
+
+    @Override
+    public Chapter get(Long refId, Long chapterId) {
+        return null;
+    }
+
+    @Override
+    public Iterable<Chapter> getAll(Long refId) {
+        return null;
     }
 }
