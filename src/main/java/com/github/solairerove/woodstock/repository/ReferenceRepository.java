8043f13c4492d2af082372207799a482a6fa04dc
solairerove <hax2033@gmail.com>
2016-11-14 12:30:24 +0300

    Update reference repository: - update insomnia export

--- src/main/java/com/github/solairerove/woodstock/repository/ReferenceRepository.java
+++ src/main/java/com/github/solairerove/woodstock/repository/ReferenceRepository.java
@@ -2,7 +2,18 @@ package com.github.solairerove.woodstock.repository;
 
 import com.github.solairerove.woodstock.domain.Reference;
 import com.github.solairerove.woodstock.repository.common.GenericRepository;
+import org.springframework.data.neo4j.annotation.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReferenceRepository extends GenericRepository<Reference> {
 
+    @Query("MATCH (module:Module)-[:HAS_REFERENCE]->(ref) " +
+            "WHERE id(module)={moduleId} AND id(ref)={refId} " +
+            "RETURN ref")
+    Reference getOneThatHasInNode(@Param("moduleId") Long moduleId, @Param("refId") Long refId);
+
+    @Query("MATCH (module:Module)-[:HAS_REFERENCE]->(refs) " +
+            "WHERE id(module)={moduleId} " +
+            "RETURN refs")
+    Iterable<Reference> getAllThatHasInNode(@Param("moduleId") Long moduleId);
 }
