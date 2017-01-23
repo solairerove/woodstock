3d7e0619258911f978599fc78d414d45e395ed43
solairerove <hax2033@gmail.com>
2017-01-23 11:15:34 +0300

    Add routes for counter and reactive form

--- woodclient/src/app/home/home.routes.ts
+++ woodclient/src/app/home/home.routes.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { ReactiveFormComponent } from './components/reactive-form/reactive-form.component';
 
 const routes: Routes = [
     {
         path: 'home', component: HomeComponent, canActivate: [],
         children: [
-            { path: 'counter', component: CounterComponent, canActivate: [] }
+            { path: 'counter', component: CounterComponent, canActivate: [] },
+            { path: 'reactive-form', component: ReactiveFormComponent, canActivate: [] }
         ]
     },
 ];
