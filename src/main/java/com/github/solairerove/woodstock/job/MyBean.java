9e1a21e5107ef61e5b84d84ef5e6353275a6dadc
¨solairerove¨ <hax2033@gmail.com>
2017-02-12 21:28:30 +0300

    Write simple job with adding unit domain

--- src/main/java/com/github/solairerove/woodstock/job/MyBean.java
+++ src/main/java/com/github/solairerove/woodstock/job/MyBean.java
@@ -1,12 +1,27 @@
 package com.github.solairerove.woodstock.job;
 
+import com.github.solairerove.woodstock.domain.Unit;
+import com.github.solairerove.woodstock.repository.UnitRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyBean implements CommandLineRunner {
 
+    private final UnitRepository repository;
+
+    @Autowired
+    public MyBean(UnitRepository repository) {
+        this.repository = repository;
+    }
+
     public void run(String... args) {
-        System.out.println("Hello from test job");
+        Unit unit = new Unit();
+        unit.setLabel("");
+        unit.setAvatar("");
+        unit.setDescription("");
+
+        System.out.println(repository.save(unit));
     }
 }
