aa780284c6497fa2f9c476905daf8e34c720ac24
solairerove <hax2033@gmail.com>
2017-03-20 23:41:08 +0300

    Write one more unit controller test: - optimize imports - add one more content type expect

--- src/test/java/com/github/solairerove/woodstock/UnitControllerTest.java
+++ src/test/java/com/github/solairerove/woodstock/UnitControllerTest.java
@@ -7,21 +7,18 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
 
 import static com.github.solairerove.woodstock.controller.ControllerApi.UNIT_API;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @SpringBootTest
@@ -43,16 +40,32 @@ public class UnitControllerTest {
     }
 
     @Test
-    public void getAllPostsTest() throws Exception {
+    public void getUnitTest() throws Exception {
+        repository.deleteAll();
+        Unit unit = new Unit("Label", "URL to avatar", "Short MD description");
+        String id = repository.save(unit).getId();
+
+        mvc.perform(request(GET, UNIT_API + "/" + id)
+                .accept(APPLICATION_JSON_UTF8_VALUE)
+                .contentType(APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.label", is("Label")))
+                .andExpect(jsonPath("$.avatar", is("URL to avatar")));;
+    }
+
+    @Test
+    public void getAllUnitsTest() throws Exception {
         repository.deleteAll();
         Unit unit = new Unit("Label", "URL to avatar", "Short MD description");
         repository.save(unit);
 
-        mvc.perform(request(HttpMethod.GET, UNIT_API)
+        mvc.perform(request(GET, UNIT_API)
                 .accept(APPLICATION_JSON_UTF8_VALUE)
                 .contentType(APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
+                .andExpect(content().contentType(APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$", hasSize(1)))
                 .andExpect(jsonPath("$.[0].label", is("Label")))
                 .andExpect(jsonPath("$.[0].avatar", is("URL to avatar")));
     }
