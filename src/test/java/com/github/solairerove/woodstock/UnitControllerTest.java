ed55abe9c5377e58af5c6ba2ca4efa57a119e145
solairerove <hax2033@gmail.com>
2017-03-23 21:54:40 +0300

    Optimize imports in unit controller test

--- src/test/java/com/github/solairerove/woodstock/UnitControllerTest.java
+++ src/test/java/com/github/solairerove/woodstock/UnitControllerTest.java
@@ -9,7 +9,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
