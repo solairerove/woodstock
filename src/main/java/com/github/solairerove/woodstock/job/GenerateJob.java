6e55a667396a7d64085cbba104366ea20a49246b
solairerove <hax2033@gmail.com>
2017-04-19 23:40:40 +0300

    Update mongo generate job: - work as previously with spring profile - delete foreach system out

--- src/main/java/com/github/solairerove/woodstock/job/GenerateJob.java
+++ src/main/java/com/github/solairerove/woodstock/job/GenerateJob.java
@@ -6,6 +6,11 @@ import com.github.solairerove.woodstock.domain.Module;
 import com.github.solairerove.woodstock.domain.Question;
 import com.github.solairerove.woodstock.domain.Reference;
 import com.github.solairerove.woodstock.domain.Unit;
+import com.github.solairerove.woodstock.repository.AnswerRepository;
+import com.github.solairerove.woodstock.repository.ChapterRepository;
+import com.github.solairerove.woodstock.repository.ModuleRepository;
+import com.github.solairerove.woodstock.repository.QuestionRepository;
+import com.github.solairerove.woodstock.repository.ReferenceRepository;
 import com.github.solairerove.woodstock.repository.UnitRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -19,86 +24,106 @@ import java.util.List;
 @Profile("generate")
 public class GenerateJob implements CommandLineRunner {
 
-    private final UnitRepository repository;
+    private final UnitRepository unitRepository;
+
+    private final ModuleRepository moduleRepository;
+
+    private final ReferenceRepository referenceRepository;
+
+    private final ChapterRepository chapterRepository;
+
+    private final QuestionRepository questionRepository;
+
+    private final AnswerRepository answerRepository;
 
     @Autowired
-    public GenerateJob(UnitRepository repository) {
-        this.repository = repository;
+    public GenerateJob(final UnitRepository unitRepository,
+                       final ModuleRepository moduleRepository,
+                       final ReferenceRepository referenceRepository,
+                       final ChapterRepository chapterRepository,
+                       final QuestionRepository questionRepository,
+                       final AnswerRepository answerRepository) {
+        this.unitRepository = unitRepository;
+        this.moduleRepository = moduleRepository;
+        this.referenceRepository = referenceRepository;
+        this.chapterRepository = chapterRepository;
+        this.questionRepository = questionRepository;
+        this.answerRepository = answerRepository;
     }
 
     public void run(String... args) {
-//
-//        int count = 0;
-//        List<Unit> units = new ArrayList<>();
-//        for (int u = 0; u < 3; u++) {
-//            Unit unit = new Unit();
-//            unit.setLabel("Unit label " + u);
-//            unit.setAvatar("Unit avatar link");
-//            unit.setDescription("Unit MD short description");
-//            count++;
-//
-//            for (int m = 0; m < 5; m++) {
-//                Module module = new Module();
-//                module.setName("Module name " + u + "->" + m);
-//                module.setAvatar("Module lint to avatar image");
-//                module.setDescription("MD short description");
-//                count++;
-//
-//                for(int q = 0; q < 35; q++) {
-//                    Question question = new Question();
-//                    question.setQuestion("Question " + "Reference title " + u + "->" + m + "->" + q);
-//                    count++;
-//
-//                    for(int a = 0; a < 10; a++) {
-//                        Answer answer = new Answer();
-//                        answer.setAnswer("Answer" + u + "->" + m + "->" + q + "->" + a);
-//                        answer.setEnable(true);
-//                        answer.setCorrect(a % 5 == 0);
-//                        count++;
-//
-//                        question.add(answer);
-//                    }
-//                    module.addQuestion(question);
-//                }
-//
-//                for (int r = 0; r < 8; r++) {
-//                    Reference reference = new Reference();
-//                    reference.setTitle("Reference title " + u + "->" + m + "->" + r);
-//                    reference.setVersion("v1");
-//                    count++;
-//
-//                    for (int c = 0; c < 10; c++) {
-//                        Chapter chapter = new Chapter();
-//                        chapter.setTitle("Chapter title " + u + "->" + m + "->" + r + "->" + c);
-//                        chapter.setContent("MD chapter content");
-//                        count++;
-//
-//                        reference.add(chapter);
-//                    }
-//                    module.addReference(reference);
-//                }
-//                unit.add(module);
-//            }
-//            units.add(unit);
-//        }
-//
-//        units.forEach(u -> {
-//            System.out.println(u.getLabel());
-//            u.getModules().forEach(m -> {
-//                System.out.println(m.getName());
-//                m.getQuestions().forEach(q -> {
-//                    System.out.println(q.getQuestion());
-//                    q.getAnswers().forEach(System.out::println);
-//                });
-//                m.getReferences().forEach(r -> {
-//                    System.out.println(r.getTitle());
-//                    r.getChapters().forEach(System.out::println);
-//                });
-//            });
-//        });
-//        System.out.println("Actually: " + count);
-//
-//        repository.deleteAll();
-//        repository.save(units);
+
+        int count = 0;
+        List<Unit> units = new ArrayList<>();
+        for (int u = 0; u < 3; u++) {
+            Unit unit = new Unit(
+                    "Unit label " + u,
+                    "Unit avatar link",
+                    "MD short description"
+            );
+            count++;
+
+            for (int m = 0; m < 5; m++) {
+                Module module = new Module(
+                        "Module name " + u + "->" + m,
+                        "Module link to avatar image",
+                        "MD short description"
+                );
+                count++;
+
+                for(int q = 0; q < 35; q++) {
+                    Question question = new Question("Question " + u + "->" + m + "->" + q);
+                    count++;
+
+                    for(int a = 0; a < 10; a++) {
+                        Answer answer = new Answer(
+                                "Answer" + u + "->" + m + "->" + q + "->" + a,
+                                true,
+                                a % 5 == 0
+                        );
+                        count++;
+
+                        answerRepository.save(answer);
+                        question.add(answer.getId());
+                    }
+                    questionRepository.save(question);
+                    module.addQuestion(question.getId());
+                }
+
+                for (int r = 0; r < 8; r++) {
+                    Reference reference = new Reference(
+                            "Reference title " + u + "->" + m + "->" + r,
+                            "v1"
+                    );
+                    count++;
+
+                    for (int c = 0; c < 10; c++) {
+                        Chapter chapter = new Chapter(
+                                "Chapter title " + u + "->" + m + "->" + r + "->" + c,
+                                "MD chapter content"
+                        );
+                        count++;
+                        chapterRepository.save(chapter);
+                        reference.add(chapter.getId());
+                    }
+                    referenceRepository.save(reference);
+                    module.addReference(reference.getId());
+                }
+                moduleRepository.save(module);
+                unit.add(module.getId());
+            }
+            units.add(unit);
+        }
+
+        System.out.println("Actually: " + count);
+
+        unitRepository.deleteAll();
+        moduleRepository.deleteAll();
+        referenceRepository.deleteAll();
+        chapterRepository.deleteAll();
+        questionRepository.deleteAll();
+        answerRepository.deleteAll();
+
+        unitRepository.save(units);
     }
 }
