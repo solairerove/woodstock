4cbc3df02462fdc702fcf79a2df03ca36334b479
solairerove <hax2033@gmail.com>
2017-04-24 23:47:35 +0300

    Fix action type name

--- woodstock/src/containers/VisibleTodoList.jsx
+++ woodstock/src/containers/VisibleTodoList.jsx
@@ -6,7 +6,7 @@ const getVisibleTodos = (todos, filter) => {
     switch (filter) {
         case 'SHOW_ALL':
             return todos;
-        case 'SHOW_COMPLETE':
+        case 'SHOW_COMPLETED':
             return todos.filter(t => t.completed);
         case 'SHOW_ACTIVE':
             return todos.filter(t => !t.completed);
