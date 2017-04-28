b5b90c476fc98db88cf49adb8b8d6d0d7661132e
solairerove <hax2033@gmail.com>
2017-04-28 23:40:10 +0300

    Delete useful containers

--- woodstock/src/components/App.jsx
+++ woodstock/src/components/App.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-import AddTodo from '../containers/AddTodo';
-import VisibleTodoList from '../containers/VisibleTodoList';
-import Footer from './Footer';
-
-const App = () => {
-    return (
+export default class App extends Component {
+    render() {
+        return (
         <div>
-            <AddTodo/>
-            <VisibleTodoList/>
-            <Footer/>
-        </div>
-    )
+            <p>Hello</p>
+        </div>    
+        );
+    };
 };
-
-export default App;
