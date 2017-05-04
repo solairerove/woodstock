08537a91d9246c4c323966a542d223f2c4b683cd
solairerove <hax2033@gmail.com>
2017-05-04 23:12:55 +0300

    Enable swagger ui rest doc

---
+++ src/main/java/com/github/solairerove/woodstock/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.github.solairerove.woodstock.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by solairerove on 5/4/17.
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket unitApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
