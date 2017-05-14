1c656ea9167b8d89f491abdc4dc26ce8aa104afa
solairerove <hax2033@gmail.com>
2017-05-14 19:39:38 +0300

    Map dispatch to props

--- woodstock/src/containers/UnitsContainer.jsx
+++ woodstock/src/containers/UnitsContainer.jsx
@@ -5,7 +5,7 @@ import AppBarHeader from '../components/AppBarHeader';
 import UnitList from '../components/UnitList';
 import * as UnitActions from '../actions/UnitActions';
 
-const UnitsContainer = ({units}) => (
+const UnitsContainer = ({units, actions}) => (
     <div>
         <AppBarHeader/>
         <UnitList units={units}/>
@@ -13,19 +13,14 @@ const UnitsContainer = ({units}) => (
 );
 
 UnitsContainer.propTypes = {
-    units: PropTypes.array.isRequired
+    units: PropTypes.array.isRequired,
+    actions: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({units: state.rootReducer.units});
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fethPosts: () => {
-            dispatch(UnitActions.fetchUnits()).then((response) => {
-                dispatch(UnitActions.fetchUnitsSuccess(response.data))
-            })
-        }
-    }
-}
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(UnitActions, dispatch)
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(UnitsContainer)
